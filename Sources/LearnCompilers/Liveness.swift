d400d1aaf40e9f45b19c926aa025ddcf4c09d8b7
Alex Nichol <unixpickle@gmail.com>
2025-11-08 19:07:10 -0500

    interference graph

--- Sources/LearnCompilers/Liveness.swift
+++ Sources/LearnCompilers/Liveness.swift
@@ -1,5 +1,26 @@
 public struct Liveness {
 
+  public struct VariableGraph: Hashable {
+    public typealias V = CFG.SSAVariable
+    public var neighbors = [V: Set<V>]()
+
+    public init() {}
+
+    public init<S: Sequence<(V, V)>>(edges: S) {
+      for (x, y) in edges {
+        insertEdge(x, y)
+      }
+    }
+
+    public mutating func insertEdge(_ from: V, _ to: V) {
+      if from == to {
+        return
+      }
+      neighbors[from, default: []].insert(to)
+      neighbors[to, default: []].insert(from)
+    }
+  }
+
   public struct Edge: Hashable {
     let from: CFG.Node
     let to: CFG.Node
@@ -103,6 +124,98 @@ public struct Liveness {
         liveOnEdge[Edge(from: node, to: $0), default: []]
       }
     )
+  }
+
+  /// Compute an interference graph for variables in a node.
+  ///
+  /// This assumes that all phi functions can be applied in parallel, and
+  /// arguments to phi functions do not interfere with their targets.
+  public func interferenceFor(node: CFG.Node) -> VariableGraph {
+    var result = VariableGraph()
+    var live = liveIn(node: node)
+    let liveOut = liveOut(node: node)
+    var onlySeenPhis = true
+    var phiDeclarations = Set<CFG.SSAVariable>()
+
+    for parent in cfg.predecessors[node, default: []] {
+      let liveChunk = liveOnEdge[.init(from: parent, to: node), default: []]
+      for x in liveChunk {
+        for y in liveChunk {
+          result.insertEdge(x, y)
+        }
+      }
+    }
+
+    for (i, inst) in cfg.nodeCode[node]!.instructions.enumerated() {
+      if case .phi = inst.op {
+        precondition(onlySeenPhis, "phi functions must all appear at the top of a node")
+        phiDeclarations.formUnion(inst.op.defs)
+      } else {
+        if onlySeenPhis {
+          // After all the phis run in parallel, we are left with the current
+          // live set, which interferes with all the phi definitions.
+          for decl in phiDeclarations {
+            for l in live {
+              result.insertEdge(decl, l)
+            }
+          }
+        }
+        onlySeenPhis = false
+      }
+
+      // Remove live variables that were used for the last time.
+      for arg in inst.op.uses {
+        if lastUses[arg]![node]! == i && !liveOut.contains(arg) {
+          live.remove(arg)
+        }
+      }
+
+      if !onlySeenPhis {
+        // Insert interference for defs and remaining live variables.
+        for arg in inst.op.defs {
+          for l in live {
+            result.insertEdge(arg, l)
+          }
+        }
+      }
+
+      // Record newly created variables as live if they are used again.
+      for arg in inst.op.defs {
+        if liveOut.contains(arg) || (lastUses[arg]?[node] ?? 0) > i {
+          live.insert(arg)
+        }
+      }
+    }
+
+    assert(live == liveOut)
+
+    // If this block only contained phi functions, we never recorded
+    // interferences between the live out set and the phi declarations.
+    if onlySeenPhis {
+      for decl in phiDeclarations {
+        for l in live {
+          result.insertEdge(decl, l)
+        }
+      }
+    }
+
+    return result
+  }
+
+  /// Create an affinity graph, where moves might be eliminated by coloring
+  /// connected vertices the same way.
+  public func affinityFor(node: CFG.Node) -> VariableGraph {
+    var result = VariableGraph()
+    for inst in cfg.nodeCode[node]!.instructions {
+      if case .phi = inst.op {
+        for dst in inst.op.defs {
+          for src in inst.op.uses {
+            result.insertEdge(dst, src)
+          }
+        }
+      }
+    }
+    return result
   }
 
 }
--- Tests/LearnCompilersTests/LivenessTest.swift
+++ Tests/LearnCompilersTests/LivenessTest.swift
@@ -87,6 +87,22 @@ import Testing
   #expect(liveness.liveOut(node: afterIf) == Set([v4]))
   #expect(liveness.liveIn(node: finalNode) == Set([v4]))
   #expect(liveness.liveOut(node: finalNode) == [])
+
+  #expect(
+    liveness.interferenceFor(node: entrypoint) == .init(edges: [(v1, v2), (v1, v4), (v2, v4)])
+  )
+  #expect(
+    liveness.interferenceFor(node: branch1) == .init(edges: [(v3v1, v4), (v1, v4)])
+  )
+  #expect(
+    liveness.interferenceFor(node: branch2) == .init(edges: [(v3v2, v4), (v2, v4)])
+  )
+  #expect(
+    liveness.interferenceFor(node: afterIf) == .init(edges: [(v3v3, v4), (v3v1, v4), (v3v2, v4)])
+  )
+  #expect(
+    liveness.interferenceFor(node: finalNode) == .init()
+  )
 }
 
 @Test func testLivenessPhiSelf() throws {
@@ -131,4 +147,163 @@ import Testing
   #expect(liveness.liveIn(node: loopBody) == Set([v1, v2]))
   #expect(liveness.liveOut(node: loopBody) == Set([v2]))
   #expect(liveness.liveOnEdge[.init(from: loopBody, to: loopBody)] == Set([v2]))
+
+  #expect(liveness.interferenceFor(node: entrypoint) == .init())
+  #expect(liveness.interferenceFor(node: loopBody) == .init())
+}
+
+@Test func testLivenessPhiMultiple() throws {
+  var cfg = CFG()
+
+  let nullPos = Position(fileID: "foo")
+
+  let v1 = CFG.SSAVariable(
+    variable: Variable(declarationPosition: nullPos, name: "v1", type: .integer)
+  )
+  let v2 = CFG.SSAVariable(
+    variable: Variable(declarationPosition: nullPos, name: "v2", type: .integer)
+  )
+  let v3 = CFG.SSAVariable(
+    variable: Variable(declarationPosition: nullPos, name: "v3", type: .integer)
+  )
+  let v4 = CFG.SSAVariable(
+    variable: Variable(declarationPosition: nullPos, name: "v4", type: .integer)
+  )
+  let v5 = CFG.SSAVariable(
+    variable: Variable(declarationPosition: nullPos, name: "v5", type: .integer)
+  )
+  let v6 = CFG.SSAVariable(
+    variable: Variable(declarationPosition: nullPos, name: "v6", type: .integer)
+  )
+  let v7 = CFG.SSAVariable(
+    variable: Variable(declarationPosition: nullPos, name: "v7", type: .integer)
+  )
+  let v8 = CFG.SSAVariable(
+    variable: Variable(declarationPosition: nullPos, name: "v8", type: .integer)
+  )
+
+  let printFunc = Function(
+    declarationPosition: nullPos, name: "print",
+    signature: .init(args: [.integer], ret: nil)
+  )
+  let addFunc = Function(
+    declarationPosition: nullPos, name: "add",
+    signature: .init(args: [.integer, .integer], ret: .integer)
+  )
+  let subFunc = Function(
+    declarationPosition: nullPos, name: "sub",
+    signature: .init(args: [.integer, .integer], ret: .integer)
+  )
+  let main = Function(
+    declarationPosition: nullPos, name: "main",
+    signature: .init(args: [.integer], ret: nil)
+  )
+
+  let entrypoint = cfg.addNode()
+  cfg.nodeCode[entrypoint] = CFG.NodeCode(instructions: [
+    .init(position: nullPos, op: .funcArg(v1, 0)),
+    .init(position: nullPos, op: .funcArg(v2, 1)),
+    .init(position: nullPos, op: .check(.variable(v1))),
+  ])
+
+  let branch1 = cfg.addNode()
+  cfg.nodeCode[branch1] = CFG.NodeCode(instructions: [
+    .init(position: nullPos, op: .callAndStore(v3, addFunc, [.variable(v1), .variable(v2)])),
+    .init(position: nullPos, op: .callAndStore(v4, subFunc, [.variable(v1), .variable(v3)])),
+  ])
+  let branch2 = cfg.addNode()
+  cfg.nodeCode[branch2] = CFG.NodeCode(instructions: [
+    .init(position: nullPos, op: .callAndStore(v5, addFunc, [.variable(v2), .variable(v1)]))
+  ])
+
+  let joinPoint = cfg.addNode()
+  cfg.nodeCode[joinPoint] = CFG.NodeCode(instructions: [
+    .init(position: nullPos, op: .phi(v6, [branch1: .variable(v3), branch2: .variable(v5)])),
+    .init(position: nullPos, op: .phi(v7, [branch1: .variable(v4), branch2: .variable(v1)])),
+    .init(position: nullPos, op: .callAndStore(v8, addFunc, [.variable(v6), .variable(v7)])),
+    .init(position: nullPos, op: .call(printFunc, [.variable(v8)])),
+    .init(position: nullPos, op: .call(printFunc, [.variable(v6)])),
+  ])
+
+  cfg.addEdge(from: entrypoint, to: .branch(ifFalse: branch1, ifTrue: branch2))
+  cfg.addEdge(from: branch1, to: .single(joinPoint))
+  cfg.addEdge(from: branch2, to: .single(joinPoint))
+  cfg.functions[main] = entrypoint
+
+  var liveness = Liveness(cfg: cfg)
+  #expect(liveness.liveIn(node: entrypoint).isEmpty)
+  #expect(liveness.liveOut(node: entrypoint) == Set([v1, v2]))
+  #expect(liveness.liveIn(node: branch1) == Set([v1, v2]))
+  #expect(liveness.liveOut(node: branch1) == Set([v3, v4]))
+  #expect(liveness.liveIn(node: branch2) == Set([v1, v2]))
+  #expect(liveness.liveOut(node: branch2) == Set([v1, v5]))
+  #expect(liveness.liveIn(node: joinPoint) == Set([v1, v3, v4, v5]))
+  #expect(liveness.liveOut(node: joinPoint) == [])
+
+  #expect(liveness.interferenceFor(node: entrypoint) == .init(edges: [(v1, v2)]))
+  #expect(liveness.interferenceFor(node: branch1) == .init(edges: [(v1, v3), (v3, v4), (v1, v2)]))
+  #expect(liveness.interferenceFor(node: branch2) == .init(edges: [(v1, v5), (v1, v2)]))
+  #expect(
+    liveness.interferenceFor(node: joinPoint)
+      == .init(edges: [(v6, v7), (v8, v6), (v1, v5), (v3, v4)])
+  )
+
+  cfg.nodeCode[joinPoint] = CFG.NodeCode(instructions: [
+    .init(position: nullPos, op: .phi(v6, [branch1: .variable(v3), branch2: .variable(v5)])),
+    .init(position: nullPos, op: .phi(v7, [branch1: .variable(v4), branch2: .variable(v1)])),
+    .init(position: nullPos, op: .callAndStore(v8, addFunc, [.variable(v6), .variable(v1)])),
+    .init(position: nullPos, op: .call(printFunc, [.variable(v8)])),
+    .init(position: nullPos, op: .call(printFunc, [.variable(v6)])),
+  ])
+
+  liveness = Liveness(cfg: cfg)
+  #expect(liveness.liveIn(node: entrypoint).isEmpty)
+  #expect(liveness.liveOut(node: entrypoint) == Set([v1, v2]))
+  #expect(liveness.liveIn(node: branch1) == Set([v1, v2]))
+  #expect(liveness.liveOut(node: branch1) == Set([v3, v4, v1]))
+  #expect(liveness.liveIn(node: branch2) == Set([v1, v2]))
+  #expect(liveness.liveOut(node: branch2) == Set([v1, v5]))
+  #expect(liveness.liveIn(node: joinPoint) == Set([v1, v3, v4, v5]))
+  #expect(liveness.liveOut(node: joinPoint) == [])
+
+  #expect(liveness.interferenceFor(node: entrypoint) == .init(edges: [(v1, v2)]))
+  #expect(
+    liveness.interferenceFor(node: branch1)
+      == .init(edges: [(v1, v3), (v1, v4), (v3, v4), (v1, v2)])
+  )
+  #expect(liveness.interferenceFor(node: branch2) == .init(edges: [(v1, v5), (v1, v2)]))
+  #expect(
+    liveness.interferenceFor(node: joinPoint)
+      == .init(edges: [
+        (v6, v7), (v6, v1), (v7, v1), (v8, v6), (v3, v4), (v3, v1), (v5, v1), (v4, v1),
+      ])
+  )
+
+  // Try separate phi and continuation
+  let finalPoint = cfg.addNode()
+
+  cfg.nodeCode[joinPoint] = CFG.NodeCode(instructions: [
+    .init(position: nullPos, op: .phi(v6, [branch1: .variable(v3), branch2: .variable(v5)])),
+    .init(position: nullPos, op: .phi(v7, [branch1: .variable(v4), branch2: .variable(v1)])),
+  ])
+  cfg.nodeCode[finalPoint] = CFG.NodeCode(instructions: [
+    .init(position: nullPos, op: .callAndStore(v8, addFunc, [.variable(v6), .variable(v1)])),
+    .init(position: nullPos, op: .call(printFunc, [.variable(v8)])),
+    .init(position: nullPos, op: .call(printFunc, [.variable(v6)])),
+  ])
+  cfg.addEdge(from: joinPoint, to: .single(finalPoint))
+
+  liveness = Liveness(cfg: cfg)
+  #expect(liveness.liveIn(node: joinPoint) == Set([v1, v3, v4, v5]))
+  #expect(liveness.liveOut(node: joinPoint) == [v1, v6])
+  #expect(liveness.liveIn(node: finalPoint) == Set([v1, v6]))
+  #expect(liveness.liveOut(node: finalPoint) == [])
+
+  #expect(
+    liveness.interferenceFor(node: joinPoint)
+      == .init(edges: [
+        (v6, v1), (v7, v1), (v7, v6), (v5, v1), (v4, v1), (v3, v1), (v3, v4),
+      ])
+  )
+  #expect(liveness.interferenceFor(node: finalPoint) == .init(edges: [(v8, v6), (v6, v1)]))
 }
